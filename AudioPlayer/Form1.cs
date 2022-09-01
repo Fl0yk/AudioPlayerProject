e03d6ad320655eb9246a0ae7d778fb2b80c6b7be
Fl0yk <kosach.dmitriy@mail.ru>
2022-09-01 16:46:46 +0300

    comments

--- AudioPlayer/Form1.cs
+++ AudioPlayer/Form1.cs
@@ -25,18 +25,16 @@ namespace AudioPlayer
   
 
             file = new StringDictionary();
-
+            
             to_image = to_image.Remove(to_image.LastIndexOf("\\"));
             to_image = to_image.Remove(to_image.LastIndexOf("\\"));
            
             to_image += "/image/";
 
-            string[] allfiles = Directory.GetFiles(Directory.GetCurrentDirectory() + "/Music", "*.mp3"); //копируем пути+названия всех песен из файла музыки
-                                                                                                         //штука сверху получает путь в Debug
+            string[] allfiles = Directory.GetFiles(Directory.GetCurrentDirectory() + "/Music", "*.mp3"); //копируем пути + названия всех песен из файла музыки
+                                                //штука сверху получает путь в Debug
             foreach (string line in allfiles)
             {
-
-                //files.Add(line);
                 int index = line.LastIndexOf("\\"); //находим индекс последней черточки из всего пути, чтоб получить название
                 string name;
                 if (index > 0)
@@ -56,11 +54,10 @@ namespace AudioPlayer
             if (listAudio.SelectedIndex != -1)//Если выюрали песню, а не тыкнули в пустое место
             {
                 label3.Text = "Играет: ";
-                label2.Text = play_now(player.URL);
                 player.URL = file[listAudio.Items[listAudio.SelectedIndex].ToString()]; //указываем путь файла с песней
                 player.Ctlcontrols.play();
                 is_play = true;
-                playPause.Image = Image.FromFile(to_image + "PAUSE.png"); //тут может быть трабл
+                playPause.Image = Image.FromFile(to_image + "PAUSE.png");
             }
         }
         
@@ -85,15 +82,15 @@ namespace AudioPlayer
             this.Close();
         }
 
-        private void collapse_Click(object sender, EventArgs e)
+        private void collapse_Click(object sender, EventArgs e) //сворачивает окно
         {
             this.WindowState = FormWindowState.Minimized;
         }
 
-        Point lastPoint;
+        Point lastPoint; 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
-            if(e.Button == MouseButtons.Left)
+            if(e.Button == MouseButtons.Left) //нажатие ЛКМ в зоне приложения
             {
                 this.Left += e.X - lastPoint.X;
                 this.Top += e.Y - lastPoint.Y;
@@ -113,7 +110,7 @@ namespace AudioPlayer
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             e.Graphics.SmoothingMode =
-            System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            System.Drawing.Drawing2D.SmoothingMode.AntiAlias; //сглаживание
 
             myPen.Color = Color.FromArgb(255, 180, 162, 132);
             myPen.Width = 7;
@@ -127,22 +124,22 @@ namespace AudioPlayer
             PointF point2 = new PointF(370, 2);
             PointF point3 = new PointF(470, 102);
         
-            PointF[] curvePoints1 =
+            PointF[] curvePoints1 = //правый треугольник
             {
                 point1,
                 point2,
                 point3,
             };
 
-            e.Graphics.FillPolygon(solidBrush, curvePoints1);
-            e.Graphics.DrawPolygon(myPen, curvePoints1);
+            e.Graphics.FillPolygon(solidBrush, curvePoints1); //закрашиваем
+            e.Graphics.DrawPolygon(myPen, curvePoints1); //обводим
 
 
             PointF point4 = new PointF(2, 2);
             PointF point5 = new PointF(2, 102);
             PointF point6 = new PointF(102, 2);
 
-            PointF[] curvePoints2 =
+            PointF[] curvePoints2 = //левый треугольник
             {
                 point4,
                 point5,
@@ -160,7 +157,7 @@ namespace AudioPlayer
             PointF point12 = new PointF(2, 240);
 
 
-            PointF[] curvePoints3 =
+            PointF[] curvePoints3 = //шестиугольник(для заднего фона)
             {
                 point7,
                 point8,
@@ -171,21 +168,21 @@ namespace AudioPlayer
 
             };
             Image myImage = Image.FromFile(to_image + "FON1.jpg");
-            TextureBrush myTextureBrush = new TextureBrush(myImage);
-            e.Graphics.FillPolygon(myTextureBrush, curvePoints3);
+            TextureBrush myTextureBrush = new TextureBrush(myImage); //инициализация заливки картинкой
+            e.Graphics.FillPolygon(myTextureBrush, curvePoints3);  //заливка шестиугольника картинкой фона
             e.Graphics.DrawPolygon(myPen, curvePoints3);
 
             myPen.Width = 4;
 
-            g.DrawArc(myPen, new Rectangle(110, 200, 30, 30), -270, 180);
+            g.DrawArc(myPen, new Rectangle(110, 200, 30, 30), -270, 180); //полуокружности
             g.DrawArc(myPen, new Rectangle(330, 200, 30, 30), -90, 180);
 
-            g.DrawLine(myPen, 125, 201, 346, 201);
+            g.DrawLine(myPen, 125, 201, 346, 201);  //линии, соединяющие окружности
             g.DrawLine(myPen, 125, 230, 346, 230);
 
-            e.Graphics.FillPie(solidBrush, new Rectangle(110, 200, 30, 30), -270, 180);
+            e.Graphics.FillPie(solidBrush, new Rectangle(110, 200, 30, 30), -270, 180); //заливка полуокружностей
             e.Graphics.FillPie(solidBrush, new Rectangle(330, 200, 30, 30), -90, 180);
-            e.Graphics.FillRectangle(solidBrush, new Rectangle(124, 201, 222, 29));
+            e.Graphics.FillRectangle(solidBrush, new Rectangle(124, 201, 222, 29)); //заливка прямоугольника
 
         }
 
@@ -212,9 +209,6 @@ namespace AudioPlayer
 
                 labelFinish.Text = player.Ctlcontrols.currentItem.durationString; //Время всей песни
                 labelNow.Text = player.Ctlcontrols.currentPositionString; //Сколько уже прошло
-
-                // label1.Text = player.playState.ToString(); //Отладочка
-                //    label1.Text = "Играет: ";
             }
             else if (player.playState == WMPLib.WMPPlayState.wmppsStopped) //Если песня закончилась
             {
@@ -227,7 +221,7 @@ namespace AudioPlayer
                     Random rnd = new Random();
                     listAudio.SelectedIndex = rnd.Next(listAudio.Items.Count);
                 }
-                else if (listAudio.SelectedIndex < listAudio.Items.Count - 1)
+                else if (listAudio.SelectedIndex < listAudio.Items.Count - 1) //если играла не последняя песня, и не выбран никакой режим
                     listAudio.SelectedIndex++;
                 else
                 {
@@ -237,7 +231,7 @@ namespace AudioPlayer
                     {
                         label2.Text = "";
                         label3.Text = "А где? :(";
-                        player.URL = ""; //мб костыль, не нашел метода, который бы обнулил путь к файлу
+                        player.URL = null;
                         progressBar.Value = 0;
                         labelFinish.Text = "00:00";
                         labelNow.Text = "00:00";
@@ -245,25 +239,25 @@ namespace AudioPlayer
                 }
             }
         }
-        private void collapse_MouseEnter(object sender, EventArgs e)
+        private void collapse_MouseEnter(object sender, EventArgs e) //наведение курсором
         {
             collapse.Image = Image.FromFile(to_image + "COLLAPSE1.png"); 
         }
 
-        private void collapse_MouseLeave(object sender, EventArgs e)
+        private void collapse_MouseLeave(object sender, EventArgs e) //курсор не наводится
         {
             collapse.Image = Image.FromFile(to_image + "COLLAPSE.png"); 
         }
 
         private void close_MouseEnter(object sender, EventArgs e)
         {
-            close.Image = Image.FromFile(to_image + "CLOSE1.png"); //тут может быть трабл
+            close.Image = Image.FromFile(to_image + "CLOSE1.png");
 
         }
 
         private void close_MouseLeave(object sender, EventArgs e)
         {
-            close.Image = Image.FromFile(to_image + "CLOSE.png"); //тут может быть трабл
+            close.Image = Image.FromFile(to_image + "CLOSE.png");
 
         }
 
@@ -314,9 +308,7 @@ namespace AudioPlayer
         {
             if (is_play)
             {
-
-                //  label1.Text = "2 " + player.playState.ToString(); //отладчик через лейбел
-                if (player.URL != "")
+                if (player.URL != null)
                 {
 
                     label3.Text = "Пауза: ";
@@ -326,13 +318,13 @@ namespace AudioPlayer
 
                 player.Ctlcontrols.pause();
                 is_play = false;
-                playPause.Image = Image.FromFile(to_image + "PLAY.png"); //тут может быть трабл
+                playPause.Image = Image.FromFile(to_image + "PLAY.png");
 
 
             }
             else
             {
-                if (player.URL != "")
+                if (player.URL != null)
                 {
 
                     label3.Text = "Играет: ";
@@ -341,7 +333,7 @@ namespace AudioPlayer
                 }
                 player.Ctlcontrols.play();
                 is_play = true;
-                playPause.Image = Image.FromFile(to_image + "PAUSE.png"); //тут может быть трабл
+                playPause.Image = Image.FromFile(to_image + "PAUSE.png");
 
             }
 
@@ -379,7 +371,7 @@ namespace AudioPlayer
         {
             if (listAudio.SelectedIndex != -1)
             {
-                File.Delete(file[listAudio.Items[listAudio.SelectedIndex].ToString()]); //Удаляем файл из основной папки(исходник живой, есл его не удаляли)
+                File.Delete(file[listAudio.Items[listAudio.SelectedIndex].ToString()]); //Удаляем файл из основной папки(исходник живой, если его не удаляли)
                 file.Remove(listAudio.Items[listAudio.SelectedIndex].ToString()); //Удаляем по названию из контейнера
 
                 if (listAudio.SelectedIndex == listAudio.Items.Count - 1)
@@ -432,13 +424,13 @@ namespace AudioPlayer
             if (is_random)
             {
                 is_random = false;
-                random.Image = Image.FromFile(to_image + "RANDOM.png"); //тут может быть трабл
+                random.Image = Image.FromFile(to_image + "RANDOM.png"); 
             }
 
             else if (!is_repeat)
             {
                 is_random = true;
-                random.Image = Image.FromFile(to_image + "RANDOM1.png"); //тут может быть трабл
+                random.Image = Image.FromFile(to_image + "RANDOM1.png");
             }
         }
 
@@ -451,13 +443,13 @@ namespace AudioPlayer
         {
             if (is_repeat)
             {
-                replay.Image = Image.FromFile(to_image + "REPLAY.png"); //тут может быть трабл
+                replay.Image = Image.FromFile(to_image + "REPLAY.png");
 
                 is_repeat = false;
             }
             else if (!is_random)
             {
-                replay.Image = Image.FromFile(to_image + "REPLAY1.png"); //тут может быть трабл
+                replay.Image = Image.FromFile(to_image + "REPLAY1.png");
                 is_repeat = true;
             }
         }
@@ -476,8 +468,5 @@ namespace AudioPlayer
             }
             return name;
         }
-
-
-
     }
 }
